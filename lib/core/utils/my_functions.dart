679b0cc275705576fe4ad5f1f2375bff93994829
Yusufdevv <yusufnurullayev03@gmail.com>
2023-08-13 00:24:30 +0500

    ref: functions are moved to myfunctions class

---
+++ lib/core/utils/my_functions.dart
@@ -0,0 +1,39 @@
+import 'package:intl/intl.dart';
+
+class MyFunctions {
+  static int realTotalDaysOfMonth(int year, int month) {
+    DateTime firstDayOfMonth = DateTime(year, month, 1);
+
+    DateTime lastDayOfMonth = DateTime(year, month + 1, 0);
+
+    // Birinchi kunni hisoblash
+    int numberOfDaysInMonth = lastDayOfMonth.day - firstDayOfMonth.day + 1;
+
+    return numberOfDaysInMonth;
+  }
+
+  static int addedDay(int totalDays, int year, int month) {
+    DateTime firstDayOfAugust = DateTime(year, month, 1);
+
+    DateFormat dayOfWeekFormat = DateFormat('EEE');
+    String firstDayOfWeek = dayOfWeekFormat.format(firstDayOfAugust);
+    switch (firstDayOfWeek) {
+      case 'Mon':
+        return 0;
+      case 'Tue':
+        return 1;
+      case 'Wed':
+        return 2;
+      case 'Thu':
+        return 3;
+      case 'Fri':
+        return 4;
+      case 'Sat':
+        return 5;
+      case 'Sun':
+        return 6;
+      default:
+        return 0;
+    }
+  }
+}
--- lib/feature/calendar/presentation/calendar_screen.dart
+++ lib/feature/calendar/presentation/calendar_screen.dart
@@ -1,54 +1,16 @@
+import 'package:calendar_widget/core/utils/my_functions.dart';
 import 'package:calendar_widget/feature/calendar/presentation/widgets/day_item_widget.dart';
 import 'package:calendar_widget/feature/calendar/presentation/widgets/week_days_part.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 @immutable
 class CalendarScreen extends StatelessWidget {
   const CalendarScreen({super.key});
 
-  int realTotalDaysOfMonth(int year, int month) {
-    DateTime firstDayOfMonth = DateTime(year, month, 1);
-
-    // Berilgan oyning oxirgi kunini olish
-    DateTime lastDayOfMonth = DateTime(year, month + 1, 0);
-
-    // Birinchi va oxirgi kunlarni hisoblash
-    int numberOfDaysInMonth = lastDayOfMonth.day - firstDayOfMonth.day + 1;
-
-    return numberOfDaysInMonth;
-  }
-
-  int addedDay(int totalDays, int year, int month) {
-    DateTime firstDayOfAugust = DateTime(year, month, 1);
-
-    // Hafta kunlarini aniqlash
-    DateFormat dayOfWeekFormat = DateFormat('EEE');
-    String firstDayOfWeek = dayOfWeekFormat.format(firstDayOfAugust);
-    switch (firstDayOfWeek) {
-      case 'Mon':
-        return 0;
-      case 'Tue':
-        return 1;
-      case 'Wed':
-        return 2;
-      case 'Thu':
-        return 3;
-      case 'Fri':
-        return 4;
-      case 'Sat':
-        return 5;
-      case 'Sun':
-        return 6;
-      default:
-        return 0;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    final totalDays = realTotalDaysOfMonth(2023, 8);
-    final addDay = addedDay(totalDays, 2023, 8);
+    final totalDays = MyFunctions.realTotalDaysOfMonth(2023, 8);
+    final addedDay = MyFunctions.addedDay(totalDays, 2023, 8);
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -66,14 +28,14 @@ class CalendarScreen extends StatelessWidget {
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                   ),
-                  semanticChildCount: totalDays + addDay,
+                  semanticChildCount: totalDays + addedDay,
                   children: List.generate(
-                    totalDays + addDay,
+                    totalDays + addedDay,
                     (index) {
-                      if (index < addDay) {
+                      if (index < addedDay) {
                         return Container();
                       }
-                      final day = index - addDay + 1;
+                      final day = index - addedDay + 1;
                       return DayItemWidget(day: day);
                     },
                   )),
